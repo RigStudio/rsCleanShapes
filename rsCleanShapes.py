7da1a18b3899b72dc80e9f457b8b3096a78aa41f
JuanLara <jlara.rigger@gmail.com>
2013-09-14 21:45:00 +0200

    This version doesn't freeze the weights maps.

--- rsCleanShapes.py
+++ rsCleanShapes.py
@@ -7,8 +7,6 @@
 import win32com.client
 from win32com.client import constants
 
-Application = win32com.client.Dispatch('XSI.Application').Application
-
 
 def XSILoadPlugin(in_reg):
     in_reg.Author = 'Juan Lara'
@@ -55,39 +53,40 @@ def rsCleanShapes_Execute(in_c_geometry):
         return False
     
     s_precision = '%.3f'
-        
     for o_geometry in c_geometry:
         Application.Logmessage('Cleaning shapes in "%s"' % o_geometry.Name, constants.siInfo)
         
         for o_cls in o_geometry.ActivePrimitive.Geometry.Clusters:
-            c_shapes = win32com.client.Dispatch('XSI.Collection')
             if str(o_cls.Properties(0)).split('.')[-1] == 'ResultClusterKey':
                 for o_shape in o_cls.LocalProperties:
                     if str(o_shape).split('.')[-1] == 'ResultClusterKey' or o_shape.type != 'clskey':
                         continue
                     
-                    c_shapes.Add(o_shape)
-                    
-                    # Crete the weightMap.
-                    
-                    o_wmap = Application.CreateWeightMap('', o_geometry, 'wm_%s' % o_shape.name, '', False)[0]
-                    o_clswmap = o_wmap.parent
-                    l_wmap = list(o_wmap.Elements.Array[0])
+                    c_wmap = win32com.client.Dispatch('XSI.Collection')
+                    for o_wmap in o_shape.NestedObjects:
+                        if o_wmap.type == 'ClusterKeyWeightMap':
+                            Application.DeactivateAbove(o_wmap.FullName, True)
+                            c_wmap.Add(o_wmap)
+        
                     l_elem = list(o_shape.Elements.Array)
-                                            
+                    for i_tmp in range(len(l_elem)):
+                        l_elem[i_tmp] = list(l_elem[i_tmp])
+
                     for i_elem in range(len(l_elem[0])):
                         l_tmp = []
                         [l_tmp.append(abs(l_elem[i_tmp][i_elem])) for i_tmp in range(0, 3)]
                         l_tmp = s_precision % sum(l_tmp)
-                        if l_tmp != s_precision % 0:
-                            l_wmap[i_elem] = 1
-                            
-                    o_wmap.Elements.Array = l_wmap
-                    
-                    Application.ApplyOp('ClsKeyWeightMapOp', '%s;%s' % (o_shape.FullName.replace('currentprops.ClsProp.', ''), o_wmap.FullName), 3, 'siPersistentOperation', '', 0)
-            
-            if c_shapes.Count != 0:
-                Application.FreezeObj(c_shapes)
-                Application.DeleteObj(o_clswmap)
+                        if l_tmp == s_precision % 0:
+                            for i_tmp in range(0, 3):
+                                l_elem[i_tmp][i_elem] = 0.0
                         
+                    o_shape.Elements.Array = l_elem
+                        
+                    for o_wmap in o_shape.NestedObjects:
+                        if o_wmap.type == 'ClsSetValuesOp':
+                            Application.FreezeObj(o_wmap)
+
+                    for o_wmap in c_wmap:
+                        Application.DeactivateAbove(o_wmap.FullName, False)
+
     return True
